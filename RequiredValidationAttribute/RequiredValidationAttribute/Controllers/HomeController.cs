288239ed5cba16d30902e48a9c613a7bf1a12956
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-08 21:44:07 +0600

    Requried validation attribute learn completed

--- RequiredValidationAttribute/RequiredValidationAttribute/Controllers/HomeController.cs
+++ RequiredValidationAttribute/RequiredValidationAttribute/Controllers/HomeController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RequiredValidationAttribute.Models;
 
 namespace RequiredValidationAttribute.Controllers
-{
+{   
+    [Route("[controller]")]
     public class HomeController : Controller
-    {
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Index(Employee e)
+        {   
+            return View();
+            //if (ModelState.IsValid)
+            //{
+            //    return "Name: " + e.Name + "\n" + "Gender: " + e.Gender + "\n" + "Age: " + e.Age;
+            //}
+            //else
+            //{
+            //    return "Data is not valid.";
+            //}
         }
     }
 }
---
+++ RequiredValidationAttribute/RequiredValidationAttribute/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RequiredValidationAttribute.Models
+{
+    public class Employee
+    {   
+        [Required(ErrorMessage ="Must insert name!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Must select gender!")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage ="Insert age please !")]
+        public int? Age { get; set; }
+    }
+}
