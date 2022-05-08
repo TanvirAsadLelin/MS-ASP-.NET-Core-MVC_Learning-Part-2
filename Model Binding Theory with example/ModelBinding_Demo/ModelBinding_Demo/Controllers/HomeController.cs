418dde86a1de7cfc92f33d93807b3d5a79b233d9
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-08 16:54:22 +0600

    Model Binding concept with theory and example completed

---
+++ Model Binding Theory with example/ModelBinding_Demo/ModelBinding_Demo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBinding_Demo.Controllers
+{   
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]/{id?}")]
+        public string Details(int id , string name)
+        {
+            return "Id: "+id +"\n" +"Name: "+name;
+        }
+    }
+}
