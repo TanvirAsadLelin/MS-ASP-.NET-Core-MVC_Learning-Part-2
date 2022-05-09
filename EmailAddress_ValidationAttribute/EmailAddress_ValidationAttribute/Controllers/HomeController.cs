31d1a7762e1d16903deb01b49e0f3be3ddc4fcbb
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-09 10:45:08 +0600

    Email address validation attribute learn completed

---
+++ EmailAddress_ValidationAttribute/EmailAddress_ValidationAttribute/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using EmailAddress_ValidationAttribute.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailAddress_ValidationAttribute.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Index(Employee e)
+        {
+            return View();
+            
+        }
+    }
+}
---
+++ EmailAddress_ValidationAttribute/EmailAddress_ValidationAttribute/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EmailAddress_ValidationAttribute.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Must enter email address!")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
