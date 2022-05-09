fb02416790be7961b8dc4ecc000a4735d67923ce
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-09 09:52:23 +0600

    String Length validation attribute learn completed

---
+++ StringLength_ValidationAttribute/StringLength_ValidationAttribute/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StringLength_ValidationAttribute.Models;
+
+namespace StringLength_ValidationAttribute.Controllers
+{   [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Index(Employee e)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ StringLength_ValidationAttribute/StringLength_ValidationAttribute/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StringLength_ValidationAttribute.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Must insert name!")]
+
+        [StringLength(15,MinimumLength =4, ErrorMessage ="Name must be within 4 and 15 characters!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Must select gender!")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Insert age please !")]
+        public int? Age { get; set; }
+    }
+}
