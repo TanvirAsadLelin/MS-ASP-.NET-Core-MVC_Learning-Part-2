5b2368d9a95046c89f78fcadf8f36527f54fc87a
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-09 16:16:48 +0600

    Regular expression validation attribute learn completed

---
+++ RegularExpression_ValidationAttribute/RegularExpression_ValidationAttribute/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegularExpression_ValidationAttribute.Models
+{
+    public class Employee
+    {
+       // string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$";
+        [Required]  
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "Use Uppercase, Lowercase, Numbers, Symbols & At least 8 Characters.")]
+        public string Password { get; set; }
+    }
+}
