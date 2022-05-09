391630a12e46539985676b9c5ad51931f64c7f38
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-09 22:14:11 +0600

    URL, Maxlength & Minlength validation attribute

---
+++ URL_MaxlengthMinlength_ValidationAttribute/URL_MaxlengthMinlength_ValidationAttribute/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using URL_MaxlengthMinlength_ValidationAttribute.Models;
+
+namespace URL_MaxlengthMinlength_ValidationAttribute.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Index(Employee e)
+        {
+            return View();
+
+        }
+    }
+}
---
+++ URL_MaxlengthMinlength_ValidationAttribute/URL_MaxlengthMinlength_ValidationAttribute/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace URL_MaxlengthMinlength_ValidationAttribute.Models
+{
+    public class Employee
+    {
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "Use Uppercase, Lowercase, Numbers, Symbols & At least 8 Characters.")]
+        [Display(Name = "Enter Password")]
+        [MaxLength(15)]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Confirm password not match with password!")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Url]
+        public string ChannelURL { get; set; }  
+    }
+}
