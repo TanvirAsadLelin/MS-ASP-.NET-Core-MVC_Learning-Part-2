7079e781c912edb8fc29ce588d51190bdcfde2f7
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-09 14:32:15 +0600

    Range validation attribute learn completed

---
+++ Range_ValidationAttribute/Range_ValidationAttribute/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Range_ValidationAttribute.Models;
+
+namespace Range_ValidationAttribute.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Index(Employee e)
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Range_ValidationAttribute/Range_ValidationAttribute/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Range_ValidationAttribute.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Please enter age!")]
+        [Range(18,60 , ErrorMessage ="Age between 18 to 60!")]
+        public int Age { get; set; }
+    }
+}
